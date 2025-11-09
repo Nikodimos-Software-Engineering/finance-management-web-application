4aa56eed3a83b884e8fcfe6d22fb4c23aa7f9c91
= <=>
2025-11-09 11:43:00 -0800

    minor bug fix

--- backend/finance/views.py
+++ backend/finance/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import viewsets, permissions
+from rest_framework import exceptions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
